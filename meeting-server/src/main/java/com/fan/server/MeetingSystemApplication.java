457c6756611c248fc17c67d7f7b8bcf820674483
kyajhl <136273344@qq.com>
2023-07-04 19:57:06 +0800

    7.4

--- meeting-server/src/main/java/com/fan/server/MeetingSystemApplication.java
+++ meeting-server/src/main/java/com/fan/server/MeetingSystemApplication.java
@@ -1,5 +1,6 @@
 package com.fan.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@MapperScan("com.fan.server.mapper")
 public class MeetingSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(MeetingSystemApplication.class, args);

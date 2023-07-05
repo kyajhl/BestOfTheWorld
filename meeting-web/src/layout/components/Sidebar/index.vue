7da6d9d22146812df364f3e6e98e22acff32b1d2
kyajhl <136273344@qq.com>
2023-07-05 14:57:53 +0800

    7.5

--- meeting-web/src/layout/components/Sidebar/index.vue
+++ meeting-web/src/layout/components/Sidebar/index.vue
@@ -30,8 +30,8 @@
       ...mapGetters(['sidebar']),
       routes() {
         // console.log(this.$router.options.routes.concat(global.myRoutes));
-        return this.$router.options.routes.concat(global.myRoutes);
-        // return this.$router.options.routes;
+        // return this.$router.options.routes.concat(global.myRoutes);
+        return this.$router.options.routes;
       },
       activeMenu() {
         const route = this.$route;

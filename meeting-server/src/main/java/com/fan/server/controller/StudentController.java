77ed310699c8670aae5bb02fff4610ed2d42c13d
kyajhl <136273344@qq.com>
2023-07-06 16:22:59 +0800

    7.6

--- meeting-server/src/main/java/com/fan/server/controller/StudentController.java
+++ meeting-server/src/main/java/com/fan/server/controller/StudentController.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * <p>
  *  前端控制器
@@ -43,9 +46,6 @@ public class StudentController {
         }
         return Result.fail(202);
     }
-
-    @Autowired
-    private IStudentService studentService;
 
     @PostMapping("/login")
     @ApiOperation("用户登录接口")

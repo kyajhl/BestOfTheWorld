43b7f833d88c3137b7c8581bf03cd026ba234b71
Xiong Ting <1149099750@qq.com>
2023-07-07 19:06:54 +0800

    xiongting

--- meeting-server/src/main/java/com/fan/server/controller/ProjectController.java
+++ meeting-server/src/main/java/com/fan/server/controller/ProjectController.java
@@ -1,6 +1,14 @@
 package com.fan.server.controller;
 
 
+import com.fan.server.common.Result;
+import com.fan.server.pojo.Project;
+import com.fan.server.pojo.User;
+import com.fan.server.service.IProjectService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/project")
 public class ProjectController {
+
+    @Autowired
+    private IProjectService projectService;
+
+    @PostMapping("/addProject")
+    @ApiOperation("创建项目接口")
+    public Result<?> addProject(@RequestBody Project project) {
+        try {
+            projectService.addProject(project);
+            return Result.success("创建项目成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail(202);
+    }
 
 }
--- meeting-server/src/main/java/com/fan/server/service/impl/ProjectServiceImpl.java
+++ meeting-server/src/main/java/com/fan/server/service/impl/ProjectServiceImpl.java
@@ -1,10 +1,21 @@
 package com.fan.server.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.fan.server.common.Result;
 import com.fan.server.pojo.Project;
 import com.fan.server.mapper.ProjectMapper;
+import com.fan.server.pojo.Student;
+import com.fan.server.pojo.User;
 import com.fan.server.service.IProjectService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Objects;
 
 /**
  * <p>
@@ -16,5 +27,19 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ProjectServiceImpl extends ServiceImpl<ProjectMapper, Project> implements IProjectService {
+
+    @Override
+    public void addProject(Project project) throws Exception {
+        // 判断 student 表是否存在 mobilePhone
+        LambdaQueryWrapper<Project> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Project::getProjectId, project.getProjectId());
+        Project project1 = this.getOne(wrapper);
+        if (!Objects.isNull(project1)) {
+            // 非空
+            throw new Exception("项目号已存在");
+        }
+        // 空直接添加
+        this.save(project);
+    }
 
 }
